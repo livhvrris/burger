626c77e971a0762035780bcc9e7aa850e2627d76
Olivia Harris <livhvrris@olivias-mbp.attlocal.net>
2017-01-27 22:50:26 -0600

    connection & db

---
+++ config/orm.js
@@ -0,0 +1,13 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+  
+}
+
+// Helper function for SQL syntax.
+function objToSql(ob) {
+  
+}
+
+
+module.exports = orm;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('McDouble');
+INSERT INTO burgers (burger_name) VALUES ('McTriple');
+INSERT INTO burgers (burger_name) VALUES ('McQuadruple');
